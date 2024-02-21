69c2c2e8dd2b803f33bc8497f8cb1a5c8cfcda7e
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-02-21 15:44:31 +0700

    feat: mantle naming

--- src/pages/address-book.tsx
+++ src/pages/address-book.tsx
@@ -6,7 +6,7 @@ const AddressBook: NextPage = () => {
   return (
     <>
       <Head>
-        <title>{'Safe{Wallet} – Address book'}</title>
+        <title>{'Mantle Safe – Address book'}</title>
       </Head>
 
       <AddressBookTable />
--- src/pages/settings/safe-apps/index.tsx
+++ src/pages/settings/safe-apps/index.tsx
@@ -9,7 +9,7 @@ const SafeAppsPermissionsPage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>{'Safe{Wallet} – Settings – Safe Apps'}</title>
+        <title>{'Mantle Safe – Settings – Safe Apps'}</title>
       </Head>
 
       <SettingsHeader />
