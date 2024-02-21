41fa1518543cd44713bf40cd35e8d4df4895d08d
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-02-21 16:24:37 +0700

    fix: a few indents

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -9,7 +9,7 @@ import packageJson from '../../../../package.json'
 // import AppstoreButton from '../AppStoreButton'
 import ExternalLink from '../ExternalLink'
 import MUILink from '@mui/material/Link'
-import { HELP_CENTER_URL, /* IS_DEV, */ IS_OFFICIAL_HOST } from '@/config/constants'
+import { /* HELP_CENTER_URL, IS_DEV, */ IS_OFFICIAL_HOST } from '@/config/constants'
 
 const footerPages = [
   AppRoutes.welcome.index,
