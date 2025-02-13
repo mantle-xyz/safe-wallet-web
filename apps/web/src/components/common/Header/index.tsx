1ec49ad084a05c47cc3aec4dd48d5dc2a7763844
ajimeno04 <jimenobeltran11@gmail.com>
2025-02-13 12:42:54 +0100

    refactor: Update header logo and remove migration banner

--- apps/web/src/components/common/Header/index.tsx
+++ apps/web/src/components/common/Header/index.tsx
@@ -23,7 +23,6 @@ import { FEATURES } from '@/utils/chains'
 import { useHasFeature } from '@/hooks/useChains'
 import Track from '@/components/common/Track'
 import { OVERVIEW_EVENTS, OVERVIEW_LABELS } from '@/services/analytics'
-import { MigrationBanner } from '../MigrationBanner'
 import { useSafeTokenEnabled } from '@/hooks/useSafeTokenEnabled'
 import { useIsOfficialHost } from '@/hooks/useIsOfficialHost'
 import { BRAND_LOGO, BRAND_NAME } from '@/config/constants'
@@ -87,7 +86,7 @@ const Header = ({ onMenuToggle, onBatchToggle }: HeaderProps): ReactElement => {
 
       <div className={classnames(css.element, css.hideMobile, css.logo)}>
         <Link href={logoHref} passHref>
-          {isOfficialHost ? <SafeLogo alt={BRAND_NAME} /> : BRAND_LOGO && <img src={BRAND_LOGO} alt={BRAND_NAME} />}
+          <SafeLogo alt="Safe logo" />
         </Link>
       </div>
 
@@ -97,9 +96,9 @@ const Header = ({ onMenuToggle, onBatchToggle }: HeaderProps): ReactElement => {
         </div>
       )}
 
-      <div className={css.element}>
+      {/* <div className={css.element}>
         <MigrationBanner />
-      </div>
+      </div> */}
 
       <div className={css.element}>
         <NotificationCenter />
