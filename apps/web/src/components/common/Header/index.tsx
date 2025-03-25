5eac77d2d1be470ec826e3766369fa663ebb4183
ajimeno04 <jimenobeltran11@gmail.com>
2025-03-25 05:46:49 +0100

    chore: update yarn.lock and refactor privacy and terms pages
    - Removed unused `useIsOfficialHost` hook from PrivacyPolicy and Terms components.
    - Simplified rendering of `SafePrivacyPolicy` and `SafeTerms` components.
    - Updated yarn.lock to remove deprecated package and add new dependencies for React and related packages.

--- apps/web/src/components/common/Header/index.tsx
+++ apps/web/src/components/common/Header/index.tsx
@@ -79,7 +79,7 @@ const Header = ({ onMenuToggle, onBatchToggle }: HeaderProps): ReactElement => {
 
       <div className={classnames(css.element, css.logoMobile)}>
         <Link href={logoHref} passHref>
-          {isOfficialHost ? <SafeLogoMobile alt="Safe logo" /> : null}
+          <SafeLogoMobile alt="Safe logo" />
         </Link>
       </div>
 
